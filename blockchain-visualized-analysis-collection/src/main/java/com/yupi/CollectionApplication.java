60106f84c7b65de9c4019ad89b42e4771c4d1d49
dhwc <505852166@qq.com>
2022-10-02 13:18:12 +0800

    初始化项目

---
+++ blockchain-visualized-analysis-collection/src/main/java/com/yupi/CollectionApplication.java
@@ -0,0 +1,15 @@
+package com.yupi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dhwc
+ * @create 2022-10-02 12:20
+ */
+@SpringBootApplication
+public class CollectionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionApplication.class, args);
+    }
+}
